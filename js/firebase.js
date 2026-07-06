0f2b12996a8c04c6522927f8d31237b1f1584b5e
golubqv <GolubYanndex@yandex.ru>
2026-07-07 00:48:25 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
@@ -19,7 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
-export { db, auth };
+export const db = getFirestore(app);

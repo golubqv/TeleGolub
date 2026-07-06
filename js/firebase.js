73e2e904663a33e4ade22a4774862aceef2ff69d
golubqv <GolubYanndex@yandex.ru>
2026-07-06 22:53:15 +0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
+  authDomain: "telegolub-app.firebaseapp.com",
+  projectId: "telegolub-app",
+  storageBucket: "telegolub-app.firebasestorage.app",
+  messagingSenderId: "153463660671",
+  appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };

f532a78540b5e6f34bccaa7c58f8c5aac5ccbbe5
golubqv <GolubYanndex@yandex.ru>
2026-07-06 16:11:54 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
-    authDomain: "telegolub-app.firebaseapp.com",
-    projectId: "telegolub-app",
-    storageBucket: "telegolub-app.firebasestorage.app",
-    messagingSenderId: "153463660671",
-    appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
+  authDomain: "telegolub-app.firebaseapp.com",
+  projectId: "telegolub-app",
+  storageBucket: "telegolub-app.firebasestorage.app",
+  messagingSenderId: "153463660671",
+  appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };

470b6996c0190795012fd46f38f10964bfa29337
golubqv <GolubYanndex@yandex.ru>
2026-07-07 00:16:37 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,43 +1,64 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { auth } from "./firebase.js";
+
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
-  authDomain: "telegolub-app.firebaseapp.com",
-  projectId: "telegolub-app",
-  storageBucket: "telegolub-app.firebasestorage.app",
-  messagingSenderId: "153463660671",
-  appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
-};
+window.register = async function(){
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const email=document.getElementById("email").value.trim();
 
-window.register = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+const password=document.getElementById("password").value;
 
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    alert("Аккаунт создан!");
-    location.href = "index.html";
-  } catch (e) {
-    alert(e.message);
-  }
-};
+if(email===""||password===""){
+alert("Заполните поля");
+return;
+}
 
-window.login = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+try{
 
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    location.href = "index.html";
-  } catch (e) {
-    alert("Неверная почта или пароль");
-  }
-};
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+location.href="setup.html";
+
+}catch(e){
+
+alert(e.message);
+
+}
+
+}
+
+window.login=async function(){
+
+const email=document.getElementById("email").value.trim();
+
+const password=document.getElementById("password").value;
+
+if(email===""||password===""){
+alert("Заполните поля");
+return;
+}
+
+try{
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+location.href="index.html";
+
+}catch{
+
+alert("Неверная почта или пароль");
+
+}
+
+}

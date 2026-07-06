d1acec57dc3c1cf8c5e05b8b94eef011d0dae4d8
golubqv <GolubYanndex@yandex.ru>
2026-07-06 18:08:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-import { db } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 import {
   collection,
@@ -12,25 +16,47 @@ import {
 const messages = document.getElementById("messages");
 const input = document.getElementById("message");
 
+// Проверяем авторизацию
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    location.href = "login.html";
+    return;
+  }
+
+  loadMessages();
+});
+
+// Отправка сообщения
 window.sendMessage = async function () {
   if (input.value.trim() === "") return;
 
   await addDoc(collection(db, "messages"), {
     text: input.value,
+    user: auth.currentUser.email,
     time: serverTimestamp()
   });
 
   input.value = "";
 };
 
-const q = query(collection(db, "messages"), orderBy("time"));
+// Загрузка сообщений
+function loadMessages() {
+  const q = query(collection(db, "messages"), orderBy("time"));
 
-onSnapshot(q, (snapshot) => {
-  messages.innerHTML = "";
+  onSnapshot(q, (snapshot) => {
+    messages.innerHTML = "";
 
-  snapshot.forEach((doc) => {
-    messages.innerHTML += `<p>${doc.data().text}</p>`;
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+
+      messages.innerHTML += `
+        <p>
+          <b>${data.user}</b><br>
+          ${data.text}
+        </p>
+      `;
+    });
+
+    messages.scrollTop = messages.scrollHeight;
   });
-
-  messages.scrollTop = messages.scrollHeight;
-});
+}

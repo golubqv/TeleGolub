81ced4e67ace98104060a200f2b8638913bb9e20
golubqv <GolubYanndex@yandex.ru>
2026-07-06 15:48:15 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
+    authDomain: "telegolub-app.firebaseapp.com",
+    projectId: "telegolub-app",
+    storageBucket: "telegolub-app.firebasestorage.app",
+    messagingSenderId: "153463660671",
+    appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>

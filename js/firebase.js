ece1f9768275aadd9b25c3df43b8145c3b585ffb
golubqv <GolubYanndex@yandex.ru>
2026-07-08 19:54:00 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,17 +4,32 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-aut
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
+import {
+    initializeAppCheck,
+    ReCaptchaV3Provider
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app-check.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
-  authDomain: "telegolub-app.firebaseapp.com",
-  projectId: "telegolub-app",
-  storageBucket: "telegolub-app.firebasestorage.app",
-  messagingSenderId: "153463660671",
-  appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
+    apiKey: "AIzaSyDLMgpqiXGxmbiTCrN7v-ukyd52BSwxihI",
+    authDomain: "telegolub-app.firebaseapp.com",
+    projectId: "telegolub-app",
+    storageBucket: "telegolub-app.firebasestorage.app",
+    messagingSenderId: "153463660671",
+    appId: "1:153463660671:web:1240cb7686be79dcf21f4a"
 };
 
 const app = initializeApp(firebaseConfig);
 
+// App Check
+initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(
+        "6Lf4eEotAAAAAHEnRWw7LE4CUu1PAQiTYb8BTzpN"
+    ),
+    isTokenAutoRefreshEnabled: true
+});
+
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
+
+export { app };
